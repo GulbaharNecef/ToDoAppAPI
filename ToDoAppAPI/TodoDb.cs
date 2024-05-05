20ce40e275a21460f4ca1480495d7dbebcabd161
Gulbahar <gnecefzade1@std.beu.edu.az>
2024-05-05 12:31:15 +0400

    Add project files.

---
+++ ToDoAppAPI/TodoDb.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ToDoAppAPI
+{
+    public class TodoDb:DbContext
+    {
+        public TodoDb(DbContextOptions<TodoDb> options): base(options)
+        {
+            
+        }
+
+        //public DbSet<Todo> Todos { get; set; }
+        public DbSet<Todo> Todos => Set<Todo>();
+    }
+}
